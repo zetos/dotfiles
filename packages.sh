cb8eeaaa28a20456198b6e0d336797fbcfb75d69
Zetos Lima <cesar_manoel@hotmail.com>
2022-08-19 20:42:54 -0300

    feat: add packages

---
+++ packages.sh
@@ -0,0 +1,81 @@
+
+echo 'Install chromium'
+yay -S chromium --noconfirm
+
+echo 'Install nvm'
+yay -S nvm --noconfirm
+
+echo 'Install docker & docker-compose'
+yay -S docker docker-compose --noconfirm
+
+echo 'Install vscode'
+yay -S visual-studio-code-bin --noconfirm
+
+echo 'Install font fira-code'
+yay -S otf-fira-code --noconfirm
+
+echo 'Install openssh'
+yay -S openssh --noconfirm
+
+echo 'Install vlc'
+yay -S vlc --noconfirm
+
+echo 'Install unzip'
+yay -S unzip --noconfirm
+
+echo 'Install pulsemixer'
+yay -S pulsemixer --noconfirm
+
+echo 'Install pamixer'
+yay -S pamixer --noconfirm
+
+echo 'Install qbittorrent'
+yay -S qbittorrent --noconfirm
+
+echo 'Install reflector'
+yay -S reflector --noconfirm
+
+echo 'Install obs-studio'
+yay -S obs-studio --noconfirm
+
+echo 'Install obs-teleport'
+yay -S obs-teleport --noconfirm
+
+echo 'Install ttf-fira-code'
+yay -S ttf-fira-code --noconfirm
+
+echo 'Install ttf-iosevka'
+yay -S ttf-iosevka --noconfirm
+
+echo 'Install noto-fonts-emoji'
+yay -S noto-fonts-emoji --noconfirm
+
+echo 'Install ngrok'
+yay -S ngrok --noconfirm
+
+echo 'Install nitrogen'
+yay -S nitrogen --noconfirm
+
+echo 'Install nordic-theme'
+yay -S nordic-theme --noconfirm
+
+echo 'Install notepadqq'
+yay -S notepadqq --noconfirm
+
+echo 'Install noto-fonts-emoji'
+yay -S noto-fonts-emoji --noconfirm
+
+echo 'Install numlockx'
+yay -S numlockx --noconfirm
+
+echo 'Install nvidia'
+yay -S nvidia --noconfirm
+
+echo 'Install nvidia-utils'
+yay -S nvidia-utils --noconfirm
+
+echo 'Install discord'
+yay -S discord --noconfirm
+
+echo 'Install lxappearance'
+yay -S lxappearance --noconfirm
